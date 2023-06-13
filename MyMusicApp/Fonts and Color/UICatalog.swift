1b627ab1b2c4852a0fb3368a81e1f69f3d402e9f
vikharevam <ViharevAMIP@gmail.com>
2023-06-13 09:45:40 +0300

    add fonts and colors
    Added a file with fonts and colors as properties. Added a guide with property names under each element. Added fonts to info file. Added icons and colors to assets

---
+++ MyMusicApp/Fonts and Color/UICatalog.swift
@@ -0,0 +1,87 @@
+//
+//  Constants.swift
+//  MyMusicApp
+//
+//  Created by Александр Вихарев on 13.06.2023.
+//
+
+
+import UIKit
+
+
+struct UICatalog {
+    
+    struct Colors {
+        static let brandBlack = UIColor(named:"Brand 1")
+        static let brandLime =  UIColor(named:"Brand 2")
+        static let lightGray = UIColor(named:"Neutral 1")
+        static let neutralGray = UIColor(named:"Neutral 2")
+        static let darkGray = UIColor(named:"Neutral 3")
+        
+    }
+    
+    struct Icons {
+        static let aspectRatio = "aspect_ratio"
+        static let off = "highlight_off"
+        static let restore = "restore"
+        static let search = "search"
+        static let settings = "settings"
+        static let thumbDown = "thumb_down"
+        static let thumbUp = "thumb_up"
+        static let error = "error_outline"
+        static let notification = "notification_important"
+        static let heart = "heart"
+        static let airPlay = "airplay"
+        static let album = "album"
+        static let libraryMusic = "library_music"
+        static let loop = "loop"
+        static let movie = "movie"
+        static let musicVideo = "music_video"
+        static let playlistAdd = "playlist_add"
+        static let queueMusic = "queue_music"
+        static let recentActors = "recent_actors"
+        static let shuffle = "shuffle"
+        static let skipNext = "skip_next"
+        static let skipPrevious = "skip_previous"
+        static let videoLibrary = "video_library"
+        static let saveAlt = "save_alt"
+        static let barChart = "bar_chart"
+        static let borderColor = "border_color"
+        static let textFields = "text_fields"
+        static let verticalAlignTop = "vertical_align_top"
+        static let verticalAlignBottom = "vertical_align_bottom"
+        static let equalizer = "Equalizer"
+        static let headset = "headset"
+        static let keyboardArrowDown = "keyboard_arrow_down"
+        static let keyboardArrowRight  = "keyboard_arrow_right"
+        static let arrowBack = "arrow_back"
+        static let share = "share"
+        
+        static let favoriteActive = "favorite_Active"
+        static let favoriteDefault = "favorite_Default"
+        static let thumbDownActive = "thumb_down_Active"
+        static let thumbDownDefault = "thumb_down_Default"
+        static let thumbUpActive = "thumb_up_Active"
+        static let thumbUpDefault = "thumb_up_Default"
+        
+        static let verticalAlignBottomActive = "vertical_align_bottom_Active"
+        static let verticalAlignBottomDefault = "vertical_align_bottom_Default"
+    }
+    
+    struct Fonts {
+        
+        static let title48 = UIFont(name: "Roboto-Bold", size: 48)
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
